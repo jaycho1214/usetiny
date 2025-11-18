ed790414e915cf6598968b01ee5bde96135f8816
Jay Cho <jaycho1214@gmail.com>
2025-11-18 15:06:51 +0900

    feat: add PostHog event tracking and configuration for user interactions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites() {
+    return [
+      {
+        source: "/relay-aqZo/static/(.*)",
+        destination: "https://us-assets.i.posthog.com/static/$1",
+      },
+      {
+        source: "/relay-aqZo/(.*)",
+        destination: "https://us.i.posthog.com/$1",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
